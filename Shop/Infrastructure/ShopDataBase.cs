7bf25722bbc0a4eb92fceddb137f77f2616f56b6
Yura Seletskiy <seletsky@hotmail.com>
2015-11-10 18:25:47 +0200

    Add new DomainModel

---
+++ Shop/Infrastructure/ShopDataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using domainShop;
+
+namespace Shop.Infrastructure
+{
+    public class ShopDataBase : DbContext
+    {
+        public ShopDataBase()
+            : base("DefaultConnection")
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
